95a07dd869f09c8fd49469759715b34f5a68f0db
Julián González <a.julian.g.30@gmail.com>
2020-04-21 01:34:40 -0500

    lista de productos en proveedores

--- src/main/java/edu/puj/patrones/cliente/domain/Provider.java
+++ src/main/java/edu/puj/patrones/cliente/domain/Provider.java
@@ -4,9 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -27,4 +26,7 @@ public class Provider extends AbstractEntity {
 
     @Column(name = "telefono")
     private String phone;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "provider")
+    private Set<Product> products;
 }
